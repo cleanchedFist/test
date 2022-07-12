89c546fda3ea6be2d2a92f2905fa96eeabfe9df0
duannaiying.dny@alibaba-inc.com <duannaiying.dny@alibaba-inc.com>
2022-07-12 17:20:52 +0800

    test

--- compileCase.js
+++ compileCase.js
@@ -1,1 +1,8 @@
-const fs = requeire('fs')
+const fs = require('fs')
+const { execSync } = require('child_process')
+const content = JSON.parse(fs.readFileSync('config.json').toString())
+content.deviceId = +content.deviceId + 1
+fs.writeFileSync('config.json', JSON.stringify(content, null, 2))
+execSync('git add config.json')
+execSync(`git commit -m 'style: 修改citest 配置'`)
+execSync(`git push`)
