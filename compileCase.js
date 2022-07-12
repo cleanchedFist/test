109375104fe17e8b68b561803d3732145e478189
duannaiying.dny@alibaba-inc.com <duannaiying.dny@alibaba-inc.com>
2022-07-12 18:10:58 +0800

    test12

--- compileCase.js
+++ compileCase.js
@@ -5,5 +5,6 @@ content.deviceId = +content.deviceId + 1
 fs.writeFileSync('config.json', JSON.stringify(content, null, 2))
 execSync('git add config.json')
 execSync(`git commit -m 'style: 修改citest 配置'`)
-execSync(`git log --pretty=one | echo`)
+const a = execSync(`git log --pretty=one`)
+console.log(a);
 // execSync(`git push --no-verify`)
