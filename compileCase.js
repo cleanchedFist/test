25725481e20b97e3d77aa2a76c11e3d30cae62b9
duannaiying.dny@alibaba-inc.com <duannaiying.dny@alibaba-inc.com>
2022-07-12 18:59:18 +0800

    feat: 23

--- compileCase.js
+++ compileCase.js
@@ -5,8 +5,8 @@ content.deviceId = +content.deviceId + 1
 fs.writeFileSync('config.json', JSON.stringify(content, null, 2))
 execSync('git add config.json')
 execSync(`git commit -m 'style: 修改citest 配置'`)
-const commitId = execSync(`git rev-parse --short HEAD`).toString()
-const branch = execSync(`git symbolic-ref --short -q HEAD`).toString()
+const commitId = execSync(`git rev-parse --short HEAD`).toString().trim()
+const branch = execSync(`git symbolic-ref --short -q HEAD`).toString().trim()
 console.log(commitId);
 console.log(branch);
 execSync(`git update-ref refs/heads/${branch} ${commitId}`)
